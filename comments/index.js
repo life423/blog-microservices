413fbf467f86fa66cc552510c95894c774f217e9
Andrew Clark <aiandrew631@gmail.com>
2024-12-03 03:20:35 -0600

    Implemented comments feature in Express server with GET and POST routes, added nodemon as start script in package.json.

---
+++ comments/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const { randomBytes } = require('crypto')
+const app = express()
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+const commentsByPostId = {}
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] || [])
+})
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({ id: commentId, content });
+    commentsByPostId[req.params.id] = comments;
+    res.status(201).send(comments);
+})
+
+const PORT = 4001
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
