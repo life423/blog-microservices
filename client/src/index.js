8807ccd339bfce7d529e145850e216d08d4c0b7f
Andrew Clark <aiandrew631@gmail.com>
2024-12-08 17:17:42 -0600

    Updated index.js to use ReactDOM's createRoot method for rendering the App component in strict mode.

--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client' // Updated import
+import App from './App'
+
+// Create a root container
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// Render the App component into the root container
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
