0ed695546e4801fbf8a887f1cfd35f8429277227
Andrew Clark <aiandrew631@gmail.com>
2024-12-08 17:26:58 -0600

    Added new PostCreate component in client/src directory.

---
+++ client/src/PostCreate.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PostCreate = () => {
+    return (
+        <div>
+            <form>
+                <div className='form-group'></div>
+            </form>
+        </div>
+    )
+}
+
+export default PostCreate
