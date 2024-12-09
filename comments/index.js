f14603e8c1f5817445e0e8b5ad7019aaaa284033
Andrew Clark <aiandrew631@gmail.com>
2024-12-08 18:46:00 -0600

    Integrated CORS middleware to handle cross-origin requests in comments service.

--- comments/index.js
+++ comments/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const { randomBytes } = require('crypto')
+const cors = require('cors');
+
 const app = express()
 
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }))
+app.use(cors());
 
 const commentsByPostId = {}
 
