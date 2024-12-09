ad0d0c4fea5885a19a4d8e3b499a8cdc98263d48
Andrew Clark <aiandrew631@gmail.com>
2024-12-08 19:19:10 -0600

    Added axios for HTTP requests and implemented onSubmit function to handle comment creation in CommentCreate component.

--- client/src/CommentCreate.js
+++ client/src/CommentCreate.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const commentCreate = ({ postId }) => {
     const [content, setContent] = useState('')
 
+    const onSubmit = async (event) => {
+        event.preventDefault()
+
+        await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+            content
+        })
+
+        setContent('')
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className='form-group'>
                     <label>New Comment</label>
                     <input
